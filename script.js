cabe4baa7f5b82e3c64a6ac45df74391773a64cb
Tiago Santos <tiagoalmeidasantos04@gmail.com>
2022-11-03 18:46:58 -0300

    To do list

---
+++ script.js
@@ -0,0 +1,84 @@
+const addTaskButton = document.getElementById("addNewTaskButton");
+const textAreaTask = document.getElementById("inputTextArea");
+const taskContainer = document.getElementById("taskContainer");
+const validationInput = () => textAreaTask.value.trim().length > 0;
+
+const addTask = () => {
+  const inputIsValid = validationInput();
+  if (!inputIsValid) {
+    return textAreaTask.classList.add("error");
+  }
+
+  const creatingTask = document.createElement("div");
+  // giving a class in div
+  creatingTask.classList = "taskitem";
+
+  const paragraph = document.createElement("p");
+  paragraph.innerText = textAreaTask.value;
+  paragraph.classList = "paragraph";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.textContent = "deletar";
+  deleteButton.classList = "deleteTaskButton";
+
+  const editButton = document.createElement("button");
+  editButton.textContent = "editar";
+  editButton.classList = "editButton";
+
+  const buttonContainer = document.createElement("div");
+  buttonContainer.classList = "buttocontainer";
+
+  buttonContainer.appendChild(editButton);
+  buttonContainer.appendChild(deleteButton);
+
+  creatingTask.appendChild(paragraph);
+  creatingTask.appendChild(buttonContainer);
+  // creatingTask.appendChild();
+
+  taskContainer.appendChild(creatingTask);
+
+  textAreaTask.value = "";
+  creatingTask.addEventListener("dblclick", () => taskIsFinished(paragraph));
+  deleteButton.addEventListener("click", () => remove(creatingTask, paragraph));
+  editButton.addEventListener("click", () => editTask(paragraph));
+};
+
+function taskIsFinished(paragraph) {
+  const tasks = taskContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(paragraph);
+
+    if (currentTaskIsBeingClicked) {
+      task.classList.add("finished");
+    }
+  }
+}
+
+function editTask(paragraph) {
+  const tasks = taskContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(paragraph);
+
+    if (currentTaskIsBeingClicked) {
+      const input = prompt("Digite sua nova tarefa");
+      const newText = input;
+      paragraph.innerText = newText;
+    }
+  }
+}
+
+function remove(creatingTask, paragraph) {
+  const tasks = taskContainer.childNodes;
+
+  for (const task of tasks) {
+    const currentTaskIsBeingClicked = task.firstChild.isSameNode(paragraph);
+
+    if (currentTaskIsBeingClicked) {
+      creatingTask.remove();
+    }
+  }
+}
+
+addTaskButton.addEventListener("click", addTask);
